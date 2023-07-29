68f6f661f2bdb93135dcc906210b6e43d234f15b
CrysoK <57516378+CrysoK@users.noreply.github.com>
2023-07-28 21:24:11 -0300

    Modifica un comentario

--- colecciones.py
+++ colecciones.py
@@ -178,8 +178,8 @@ class Canales(Document):
 class Mensajes(Document):
     """
     Datos de mensajes que el bot debe observar constantemente para cumplir con
-    ciertas funciones. Los id de los mensajes son únicos para todo Discord por
-    lo que no es necesario el id del espacio ni del canal al que pertenecen.
+    ciertas funciones. Los id de los mensajes son únicos para todo Discord (por
+    lo que no es necesario el id del espacio ni del canal al que pertenecen)?.
     """
 
     _id = IntField(primary_key=True)
